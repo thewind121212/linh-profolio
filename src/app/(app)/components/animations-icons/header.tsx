b8ea364a23b49a325eec1d5a07b16b29fb24de7d
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-11-18 19:45:52 +0700

    fix type error

--- src/app/(app)/components/animations-icons/header.tsx
+++ src/app/(app)/components/animations-icons/header.tsx
@@ -4,8 +4,8 @@ import Lottie from 'lottie-react';
 import Home from './lottie-animations/home.json'
 import About from './lottie-animations/about.json'
 import Blog from './lottie-animations/blogs.json'
-import Work from './lottie-animations/Tech.json'
-import Contact from './lottie-animations/Contact.json'
+import Work from './lottie-animations/tech.json'
+import Contact from './lottie-animations/contact.json'
 
 
 
