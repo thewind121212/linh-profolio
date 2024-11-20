f7f39f4ce4157159188c31029f6a86616b749fc0
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-11-20 19:42:13 +0700

    build the header menu change color

---
+++ src/app/(app)/share-components/TextHover.tsx
@@ -0,0 +1,18 @@
+
+type Props = {
+  content: string
+  className?:string
+}
+
+export default function TextHover({content, className = ''}: Props) {
+  return (
+<div className={`h-6 items-center text-black no-underline flex group ${className}`}>
+  <span className="relative h-4 overflow-hidden p-0">
+    <div className="transition-transform duration-[0.4s] ease-[ease] group-hover:-translate-y-4">
+      <span className="origin-[right_center] block text-[16px] leading-4 transition-transform duration-[0.4s] ease-[ease] group-hover:rotate-[20deg]">{content}</span>
+      <span className="origin-[left_center] rotate-[20deg] block text-[16px] leading-4 transition-transform duration-[0.4s] ease-[ease] group-hover:rotate-0">{content}</span>
+    </div>
+  </span>
+</div>
+  )
+}
